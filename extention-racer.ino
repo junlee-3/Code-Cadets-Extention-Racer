7108c4627c36b2136a27cf68084ab2471c6170f5
Jun Lee <82641992+junlee-3@users.noreply.github.com>
2023-05-06 09:34:16 +1000

    Update extention-racer.ino
    Added emergency stop button, and an ultrasonic sensor at the front of the car, so when an object is too close to the car it will stop

--- extention-racer.ino
+++ extention-racer.ino
@@ -6,9 +6,15 @@
 #include <Arduino_JSON.h>
 #include <Servo.h> // Add the Servo library
 
+#define trigPin D5
+#define echoPin D6
+
+Servo steering;
+Servo ESC;
+
 // Replace with your network credentials
-const char* ssid = "ssid";
-const char* password = "password";
+const char* ssid = "CC-Ext";
+const char* password = "CCRasPiNetwork";
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
@@ -20,91 +26,97 @@ const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>ESP Car</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/png" href="favicon.png">
-    <link rel="stylesheet" type="text/css" href="style.css">
+<title>ESP Car</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" type="image/png" href="favicon.png">
+<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <div class="topnav">
-        <h1>ESP Car</h1>
-    </div>
-    <div class="content">
-        <div class="card-grid">
-            <div class="card">
-                <p class="card-title">Steering</p>
-                <p class="switch">
-                    <input type="range" onchange="updateSliderPWM(this)" id="slider1" min="0" max="180" step="1" value ="0" class="slider">
-                </p>
-                <p class="state">Angle: <span id="sliderValue1"></span></p>
-            </div>
-            <div class="card">
-                <p class="card-title">Speed</p>
-                <p class="switch">
-                    <input type="range" onchange="updateSliderPWM(this)" id="slider2" min="0" max="180" step="1" value ="0" class="slider">
-                </p>
-                <p class="state">Speed: <span id="sliderValue2"></span></p>
-            </div>
-        </div>
-    </div>
-    <script>
+<div class="topnav">
+<h1>ESP Car</h1>
+</div>
+<div class="content">
+<div class="card-grid">
+<div class="card">
+<button onclick="emergencyBreak()" style="background-color: red; color: white;">STOP!!!</button>
+</div>
+<div class="card">
+<p class="card-title">Steering</p>
+<p class="switch">
+<input type="range" oninput="updateSliderPWM(this)" id="slider1" min="0" max="180" step="1" value ="0" class="slider">
+</p>
+<p class="state">Angle: <span id="sliderValue1"></span></p>
+</div>
+<div class="card">
+<p class="card-title">Speed</p>
+<p class="switch">
+<input type="range" oninput="updateSliderPWM(this)" id="slider2" min="0" max="180" step="1" value ="0" class="slider" style="width: 500px;">
+</p>
+<p class="state">Speed: <span id="sliderValue2"></span></p>
+</div>
+</div>
+</div>
+<script>
 
 var gateway = `ws://${window.location.hostname}/ws`;
 var websocket;
 window.addEventListener('load', onload);
 
 function onload(event) {
-    initWebSocket();
+initWebSocket();
 }
 
 function getValues(){
-    websocket.send("getValues");
+websocket.send("getValues");
 }
 
 function initWebSocket() {
-    console.log('Trying to open a WebSocket connection…');
-    websocket = new WebSocket(gateway);
-    websocket.onopen = onOpen;
-    websocket.onclose = onClose;
-    websocket.onmessage = onMessage;
+console.log('Trying to open a WebSocket connection…');
+websocket = new WebSocket(gateway);
+websocket.onopen = onOpen;
+websocket.onclose = onClose;
+websocket.onmessage = onMessage;
 }
 
 function onOpen(event) {
-    console.log('Connection opened');
-    getValues();
+console.log('Connection opened');
+getValues();
 }
 
 function onClose(event) {
-    console.log('Connection closed');
-    setTimeout(initWebSocket, 2000);
+console.log('Connection closed');
+setTimeout(initWebSocket, 2000);
 }
 
 function updateSliderPWM(element) {
-    var sliderNumber = element.id.charAt(element.id.length-1);
-    var sliderValue = document.getElementById(element.id).value;
-    document.getElementById("sliderValue"+sliderNumber).innerHTML = sliderValue;
-    console.log(sliderValue);
-    websocket.send(sliderNumber+"s"+sliderValue.toString());
+var sliderNumber = element.id.charAt(element.id.length-1);
+var sliderValue = document.getElementById(element.id).value;
+document.getElementById("sliderValue"+sliderNumber).innerHTML = sliderValue;
+console.log(sliderValue);
+websocket.send(sliderNumber+"s"+sliderValue.toString());
 }
 
 function onMessage(event) {
-    console.log(event.data);
-    var myObj = JSON.parse(event.data);
-    var keys = Object.keys(myObj);
+console.log(event.data);
+var myObj = JSON.parse(event.data);
+var keys = Object.keys(myObj);
 
-    for (var i = 0; i < keys.length; i++){
-        var key = keys[i];
-        document.getElementById(key).innerHTML = myObj[key];
-        document.getElementById("slider"+ (i+1).toString()).value = myObj[key];
-    }
+for (var i = 0; i < keys.length; i++){
+var key = keys[i];
+document.getElementById(key).innerHTML = myObj[key];
+document.getElementById("slider"+ (i+1).toString()).value = myObj[key];
 }
-    </script>
+}
+
+function emergencyBreak() {
+websocket.send("Stop");
+}
+</script>
 </body>
 </html>
 )rawliteral";
 
 // Create Servo objects
-
 
 String message = "";
 String sliderValue1 = "0";
@@ -113,125 +125,151 @@ String sliderValue2 = "0";
 int dutyCycle1;
 int dutyCycle2;
 
+long duration;
+long distance;
+
 //Json Variable to Hold Slider Values
 JSONVar sliderValues;
 
 //Get Slider Values
 String getSliderValues(){
-  sliderValues["sliderValue1"] = String(sliderValue1);
-  sliderValues["sliderValue2"] = String(sliderValue2);
+sliderValues["sliderValue1"] = String(sliderValue1);
+sliderValues["sliderValue2"] = String(sliderValue2);
 
-  String jsonString = JSON.stringify(sliderValues);
-  return jsonString;
-}
-
-// Initialize LittleFS
-void initFS() {
-  if (!LittleFS.begin()) {
-    Serial.println("An error has occurred while mounting LittleFS");
-  }
-  else{
-   Serial.println("LittleFS mounted successfully");
-  }
+String jsonString = JSON.stringify(sliderValues);
+return jsonString;
 }
 
 // Initialize WiFi
 void initWiFi() {
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi ..");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('.');
-    delay(1000);
-  }
-  Serial.println(WiFi.localIP());
+WiFi.begin(ssid, password);
+Serial.print("Connecting to WiFi ..");
+while (WiFi.status() != WL_CONNECTED) {
+Serial.print('.');
+delay(1000);
+}
+Serial.println(WiFi.localIP());
 }
 
 void notifyClients(String sliderValues) {
-  ws.textAll(sliderValues);
-  Serial.println(sliderValues);
+ws.textAll(sliderValues);
+Serial.println(sliderValues);
+}
+
+void updateSteering() {
+steering.write(dutyCycle1);
+}
+
+void updateESC() {
+ESC.write(dutyCycle2);
+}
+
+void emergencyBreak() {
+ESC.write(0);
 }
 
 void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
-  AwsFrameInfo *info = (AwsFrameInfo*)arg;
-  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
-    data[len] = 0;
-    message = (char*)data;
-    if (message.indexOf("1s") >= 0) {
-      sliderValue1 = message.substring(2);
-      dutyCycle1 = map(sliderValue1.toInt(), 0, 180, 0, 180); // modify the map function to match the servo range (0-180)
-      //Do something
-      Serial.println(dutyCycle1);
-      Serial.print(getSliderValues());
-      notifyClients(getSliderValues());
-    }
-    if (message.indexOf("2s") >= 0) {
-      sliderValue2 = message.substring(2);
-      dutyCycle2 = map(sliderValue2.toInt(), 0, 180, 0, 180);
-      //Do something
-      Serial.println(dutyCycle2);
-      Serial.print(getSliderValues());
-      notifyClients(getSliderValues());
-    }
-    if (strcmp((char*)data, "getValues") == 0) {
-      notifyClients(getSliderValues());
-    }
-  }
+AwsFrameInfo *info = (AwsFrameInfo*)arg;
+if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+data[len] = 0;
+message = (char*)data;
+if (message.indexOf("1s") >= 0) {
+sliderValue1 = message.substring(2);
+dutyCycle1 = map(sliderValue1.toInt(), 0, 180, 0, 180); // modify the map function to match the servo range (0-180)
+//Do something
+Serial.println(dutyCycle1);
+updateSteering();
+Serial.print(getSliderValues());
+notifyClients(getSliderValues());
+}
+if (message.indexOf("2s") >= 0) {
+sliderValue2 = message.substring(2);
+dutyCycle2 = map(sliderValue2.toInt(), 0, 180, 0, 180);
+//Do something
+Serial.println(dutyCycle2);
+updateESC();
+Serial.print(getSliderValues());
+notifyClients(getSliderValues());
+}
+if (strcmp((char*)data, "getValues") == 0) {
+notifyClients(getSliderValues());
+}
+if (message.indexOf("Stop") >= 0) {
+emergencyBreak();
+}
+}
 }
 void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
-  switch (type) {
-    case WS_EVT_CONNECT:
-      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
-      break;
-    case WS_EVT_DISCONNECT:
-      Serial.printf("WebSocket client #%u disconnected\n", client->id());
-      break;
-    case WS_EVT_DATA:
-      handleWebSocketMessage(arg, data, len);
-      break;
-    case WS_EVT_PONG:
-    case WS_EVT_ERROR:
-      break;
-  }
+switch (type) {
+case WS_EVT_CONNECT:
+Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+break;
+case WS_EVT_DISCONNECT:
+Serial.printf("WebSocket client #%u disconnected\n", client->id());
+break;
+case WS_EVT_DATA:
+handleWebSocketMessage(arg, data, len);
+break;
+case WS_EVT_PONG:
+case WS_EVT_ERROR:
+break;
+}
 }
 
 void initWebSocket() {
-  ws.onEvent(onEvent);
-  server.addHandler(&ws);
-  Serial.println("WebSocket");
+ws.onEvent(onEvent);
+server.addHandler(&ws);
+Serial.println("WebSocket");
 }
 
 void setup() {
-  Serial.begin(9600);
+Serial.begin(9600);
 
-  // Set servo pins
-  const int servoPin1 = 5;
-  const int servoPin2 = 16;
+// pinMode(trigPin, OUTPUT);
+// pinMode(echoPin, INPUT);
 
-  // Set servo object
+// Set servo pins
+const int servoPin1 = 5;
+const int servoPin2 = 16;
 
-  // Attach servos to pins
+// Set servo object
 
-  initFS();
-  initWiFi();
-  initWebSocket();
+// Attach servos to pins
+steering.attach(D1);
+ESC.attach(D2, 1000, 2000);
 
-  // Web Server Root URL
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/html", index_html);
-  });
+initWiFi();
+initWebSocket();
 
-  server.serveStatic("/", LittleFS, "/");
+// Web Server Root URL
+server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+request->send(200, "text/html", index_html);
+});
 
-  // Start server
-  server.begin();
+// server.serveStatic("/", LittleFS, "/");
+
+// Start server
+server.begin();
 }
 
-
 void loop() {
-  // Set servo angles based on duty cycle values
-  
-  //Do something
+// Reset trigPin
+// digitalWrite(trigPin, LOW);
+// delayMicroseconds(2);
+// // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+// digitalWrite(trigPin, HIGH);
+// delayMicroseconds(10);
+// digitalWrite(trigPin, LOW);
+// // Reads the echoPin, returns the sound wave travel time in microseconds
+// duration = pulseIn(echoPin, HIGH);
+// // Calculating the distance
+// distance = duration * 0.034 / 2; // Speed of sound wave (in cm/s) divided by 2 (go and back)
+// Serial.print("Distance: ");
+// Serial.println(distance);
+// if (distance < 30) {
+// emergencyBreak();
+// }
 
-  // Cleanup WebSocket clients
-  ws.cleanupClients();
+// Cleanup WebSocket clients
+ws.cleanupClients();
 }
